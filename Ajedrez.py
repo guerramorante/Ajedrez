c574edc5ac6beae56d02d77022c0a9c34e3d772b
guerramorante <mmorante@myuax.com>
2021-12-11 14:48:27 +0100

    commit

---
+++ Ajedrez.py
@@ -0,0 +1,2 @@
+tablero_inicial = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
+tablero = []
